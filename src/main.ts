ea35a4af85e23703804cecadca429c26645f80b1
Marichka0406 <am.kurhanevych@gmail.com>
2025-04-06 18:41:24 +0300

    Added router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import router from './router'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
